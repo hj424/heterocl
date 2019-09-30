92ef91ba3a8451527367bf9f29f7ddb2a8a2b1bd
hj424 <hj424@cornell.edu>
2019-09-30 16:53:51 -0400

    add forfirrtl example

---
+++ samples/firrtl_backend/for_addition.py
@@ -0,0 +1,16 @@
+import heterocl as hcl
+
+hcl.init()
+a = hcl.placeholder((), "a")
+
+def for_loop(a):
+  with hcl.Stage("S"):
+    b = hcl.local(a, "b")
+    with hcl.for_(0, 5, name="i") as i:
+      b[0] = b[0] + 1
+    return b
+
+s = hcl.create_schedule([a], for_loop)
+print(hcl.lower(s))
+f = hcl.build(s, target='forfirrtl')
+print(f)
