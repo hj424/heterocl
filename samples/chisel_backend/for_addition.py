e51b8f7f65ae6ad55c0c2426ab7192c3d8f6702b
hj424 <hj424@cornell.edu>
2019-10-30 13:58:41 -0400

    add comments to the example

--- samples/chisel_backend/for_addition.py
+++ samples/chisel_backend/for_addition.py
@@ -1,16 +1,22 @@
+# import heterocl package
 import heterocl as hcl
 
+# initialize the hcl project
 hcl.init()
+# constructure hcl placeholder for inputs/outputs
 a = hcl.placeholder((), "a")
-
+# the computing function
 def for_loop(a):
   with hcl.Stage("S"):
     b = hcl.local(a, "b")
     with hcl.for_(0, 5, name="i") as i:
       b[0] = b[0] + 1
     return b
-
+# create a schedule for compute optimization
 s = hcl.create_schedule([a], for_loop)
+# print out the lowered function, i.e., hcl IR
 print(hcl.lower(s))
+# generate the code for a specific backend
 f = hcl.build(s, target='chisel', name="for_addition")
+# print out the generated code
 print(f)
